4f64e28716ba810ae1f22a9cc3e7cec5bde0e736
Susheel Kumar <97793079+susheel7783@users.noreply.github.com>
2026-01-01 02:00:10 +0530

    Update variables.go

--- 3_variables/variables.go
+++ 3_variables/variables.go
@@ -1,18 +1,18 @@
 package main
 
 func main() {
-	// var name string = "golang"
+	// var name string = "golang"      
 
 	// infer
-	// var name = "golang"
+	// var name = "golang"          --no need to write string it will automatically understood
 	// var isAdult bool = true
 
 	// var age int = 30
 
 	// shorthand syntax
-	// name := "golang"
+	// name := "golang"     --in short we can write like this 
 
-	// var name string
+	// var name string     --but if we are declaring only then we have to mention string
 	// name = "golang"
 
 	// var price float32 = 50.5
