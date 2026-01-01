51722dad8c57e686db9d6a7465462cc68512ece4
Susheel Kumar <97793079+susheel7783@users.noreply.github.com>
2026-01-02 02:29:05 +0530

    Update maps.go

--- 10_maps/maps.go
+++ 10_maps/maps.go
@@ -4,7 +4,7 @@ package main
 func main() {
 	// creating map
 
-	// m := make(map[string]string)
+	// m := make(map[string]string)  --key- string, value- string 
 
 	// setting an element
 	// m["name"] = "golang"
@@ -14,21 +14,21 @@ func main() {
 	// fmt.Println(m["name"], m["area"])
 	// IMP: if key does not exists in the map then it returns zero value
 
-	// m := make(map[string]int)
+	// m := make(map[string]int) --key- string, value- int
 	// m["age"] = 30
 	// m["price"] = 50
 	// fmt.Println(m["phone"])
 	// fmt.Println(len(m))
 
 	// delete(m, "price")
-	// clear(m)
+	// clear(m)  -- we can empty the map
 
 	// fmt.Println(m)
 	// fmt.Println(m)
 
-	// m := map[string]int{"price": 40, "phones": 3}
+	// m := map[string]int{"price": 40, "phones": 3}  -- we can make map without using make fun
 
-	// v, ok := m["phones"]
+	// v, ok := m["phones"]    --first var give value and 2nd give true or  false
 	// fmt.Println(v)
 	// if ok {
 	// 	fmt.Println("all ok")
