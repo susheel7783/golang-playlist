0dc69d8d98218c6bff8c0671fb776edcda940015
Susheel Kumar <97793079+susheel7783@users.noreply.github.com>
2026-01-02 12:55:55 +0530

    Update range.go

--- 11_range/range.go
+++ 11_range/range.go
@@ -5,11 +5,29 @@ import "fmt"
 // iterating over data structures
 func main() {
 	// nums := []int{6, 7, 8}
+    // for i:=0;i<len(nums);i++{
+   // 	fmt.Println(nums[i])  --op 6 7 8 
+   // }
 
-	// for i, num := range nums {
+
+	//we will iterate using range
+
+	// for _, num := range nums {
+	// 	fmt.Println(num)           --op 6 7 8
+	// }
+
+	
+	// for i, num := range nums {    -- i is basically Index 
 	// 	fmt.Println(num, i)
 	// }
 
+	// sum:=0
+	// for _, num := range nums {
+	// 	sum=sum+num
+	// }
+   // fmt.Println(sum)         --21
+
+	
 	// m := map[string]string{"fname": "john", "lname": "doe"}
 
 	// for k, v := range m {
@@ -17,15 +35,19 @@ func main() {
 	// 	fmt.Println(k, v)
 	// }
 
-	// for k := range m {
+	// for k := range m {    --it will give only keys
 	// 	fmt.Println(k)
 	// }
 
 	// unicode code point rune
 	// starting byte of rune
 	// 300 -> 1 byte , 2 byte
+// for i, c := range "golang" {
+// 		fmt.Println(i, c)             --this will give unicode of the cahr
+// 	}
+	
 	for i, c := range "golang" {
-		fmt.Println(i, string(c))
+		fmt.Println(i, string(c))   --this will give char
 	}
 
 }
