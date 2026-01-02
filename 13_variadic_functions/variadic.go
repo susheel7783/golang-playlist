e15168bda021a25367098554bdd9ffe45ce3c3da
Susheel Kumar <97793079+susheel7783@users.noreply.github.com>
2026-01-03 01:25:18 +0530

    Update variadic.go

--- 13_variadic_functions/variadic.go
+++ 13_variadic_functions/variadic.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func sum(nums ...int) int {
+func sum(nums ...int) int {    --here we are passing n numbers of parameters using (...) and passing all the parameter are int
 	total := 0
 
 	for _, num := range nums {
@@ -13,6 +13,9 @@ func sum(nums ...int) int {
 }
 
 func main() {
+    fmt.Println(1,2,3,4,5,66, "Hi")     --we can pass anything here, this is a variadic function, we can pass n numbers of parameters
+	result:=sum(1,2,3,6,7)  --here  we can pass any number of parameters 
+	
 	nums := []int{3, 4, 5, 6}
 	result := sum(nums...)
 	fmt.Println(result)
