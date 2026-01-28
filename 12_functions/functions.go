5f37ff2c9328992d345885a7abbf1079b7cb4ad9
Susheel Kumar <97793079+susheel7783@users.noreply.github.com>
2026-01-29 02:35:36 +0530

    Update functions.go

--- 12_functions/functions.go
+++ 12_functions/functions.go
@@ -1,38 +1,86 @@
 package main
 
-// func add(a int, b int) int {
-// 	return a + b
-// }
+import "fmt"
 
-// func add(a, b int) int {   --if both are int then we can write like this
-// 	return a + b
-// }
+// -----------------------------------
+// 1. Normal function
+// -----------------------------------
+func add(a int, b int) int {
+	return a + b
+}
 
-// func getLanguages() (string, string, bool) {
-// 	return "golang", "javascript", true
-// }
+// if both parameters are same type, we can write like this:
+// func add(a, b int) int { ... }
 
-// func processIt(fn func(a int) int) {
-// 	fn(1)
-// }
+// -----------------------------------
+// 2. Multiple return values
+// -----------------------------------
+func getLanguages() (string, string, bool) {
+	return "golang", "javascript", true
+}
 
-// func processIt() func(a int) int {
-// 	return func(a int) int {
-// 		return 4
-// 	}
-// }
+// -----------------------------------
+// 3. Function as parameter (callback)
+// -----------------------------------
+func processIt(fn func(a int) int) {
+	result := fn(1)
+	fmt.Println("callback result:", result)
+}
+
+// -----------------------------------
+// 4. Function returning a function
+// -----------------------------------
+func processIt2() func(a int) int {
+	return func(a int) int {
+		return a * 2
+	}
+}
 
 func main() {
-	// fn := func(a int) int {
-	// 	return 2
-	// }
 
-	fn := processIt()
-	fn(6)
-   // fmt.Printlm(getLanguages()) 
-	//let's first get and then print(below code)
-	// lang1, lang2, _ := getLanguages()    --if we don't want to use lang3 then we are putting underscore(_)
-	// fmt.Println(lang1, lang2)
-	// result := add(3, 5)
-	// fmt.Println(result)
+	// -----------------------------------
+	// Anonymous function assigned to variable
+	// -----------------------------------
+	fn := func(a int) int {
+		return a + 10
+	}
+
+	processIt(fn) // passing function as argument
+
+	// -----------------------------------
+	// Function returning function
+	// -----------------------------------
+	newFn := processIt2()
+	fmt.Println("returned function result:", newFn(6))
+
+	// -----------------------------------
+	// Multiple return values
+	// -----------------------------------
+	lang1, lang2, ok := getLanguages()
+	fmt.Println(lang1, lang2, ok)
+
+	// ignore one return value using _
+	l1, l2, _ := getLanguages()
+	fmt.Println(l1, l2)
+
+	// -----------------------------------
+	// Normal function call
+	// -----------------------------------
+	result := add(3, 5)
+	fmt.Println("add result:", result)
 }
+// ----------------
+✅ Normal function
+func add(a, b int) int
+
+✅ Multiple returns
+func test() (int, string, bool)
+
+✅ Function as argument
+func process(fn func(int) int)
+
+✅ Function returning function
+func getFn() func(int) int
+
+✅ Anonymous function
+fn := func(a int) int { return a }
