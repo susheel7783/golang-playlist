4b44e86253b2229b8bee781a45bb0f03b60c6234
Susheel Kumar <97793079+susheel7783@users.noreply.github.com>
2026-01-28 20:46:06 +0530

    Update switch.go

--- 7_switch/switch.go
+++ 7_switch/switch.go
@@ -1,58 +1,73 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
-	// simple switch
-	// i := 3
-	// switch i {
-	// case 1:
-	// 	fmt.Println("one")    --in go no need to write break here, go automatically break it
-	// case 2:
-	// 	fmt.Println("two")
-	// case 3:
-	// 	fmt.Println("three")
-	// default:
-	// 	fmt.Println("other")
-	// }
 
-	// multiple condition switch
+	// -----------------------------------
+	// 1. Simple switch
+	// Go automatically breaks after each case
+	// -----------------------------------
+	i := 3
+	switch i {
+	case 1:
+		fmt.Println("one") // no need to write break in Go
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	default:
+		fmt.Println("other")
+	}
 
-	// switch time.Now().Weekday() {
-	// case time.Saturday, time.Sunday:
-	// 	fmt.Println("it's weekend")
-	// default:
-	// 	fmt.Println("it's workday")
-	// }
+	// -----------------------------------
+	// 2. Multiple values in a case
+	// -----------------------------------
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("it's weekend")
+	default:
+		fmt.Println("it's workday")
+	}
 
-	// type switch
-	// whoAmI := func(i interface{}) {
-	// 	switch i.(type) {
-	// 	case int:
-	// 		fmt.Println("its an integer")
-	// 	case string:
-	// 		fmt.Println("its a string")
-	// 	case bool:
-	// 		fmt.Println("its a boolean")
-	// 	default:
-	// 		fmt.Println("other")
-	// 	}
-	// }
+	// -----------------------------------
+	// 3. Type switch (detects data type)
+	// -----------------------------------
+	whoAmI := func(i interface{}) {
+		switch i.(type) {
+		case int:
+			fmt.Println("it's an integer")
+		case string:
+			fmt.Println("it's a string")
+		case bool:
+			fmt.Println("it's a boolean")
+		default:
+			fmt.Println("other type")
+		}
+	}
 
-	// whoAmI(55)   --calling the function
-	
-	//------2nd case-------------
-	// whoAmI := func(i interface{}) {
-	// 	switch t=i.(type) {
-	// 	case int:
-	// 		fmt.Println("its an integer")
-	// 	case string:
-	// 		fmt.Println("its a string")
-	// 	case bool:
-	// 		fmt.Println("its a boolean")
-	// 	default:
-	// 		fmt.Println("other",t)
-	// 	}
-	// }
+	whoAmI(55)
+	whoAmI("golang")
+	whoAmI(true)
 
-	// whoAmI(55)   --calling the function
+	// -----------------------------------
+	// 4. Type switch with variable
+	// -----------------------------------
+	whoAmI2 := func(i interface{}) {
+		switch t := i.(type) { // t holds the actual value
+		case int:
+			fmt.Println("integer:", t)
+		case string:
+			fmt.Println("string:", t)
+		case bool:
+			fmt.Println("boolean:", t)
+		default:
+			fmt.Println("other:", t)
+		}
+	}
 
+	whoAmI2(99.5)
 }
