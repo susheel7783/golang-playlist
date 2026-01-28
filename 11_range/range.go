d075879979f63451532e87a731b2936d63a667ab
Susheel Kumar <97793079+susheel7783@users.noreply.github.com>
2026-01-29 02:22:12 +0530

    Update range.go

--- 11_range/range.go
+++ 11_range/range.go
@@ -2,52 +2,86 @@ package main
 
 import "fmt"
 
-// iterating over data structures
+// Iterating over data structures using for and range
+
 func main() {
-	// nums := []int{6, 7, 8}
-    // for i:=0;i<len(nums);i++{
-   // 	fmt.Println(nums[i])  --op 6 7 8 
-   // }
 
+	// -----------------------------------
+	// 1. Iterating over a slice (classic loop)
+	// -----------------------------------
+	nums := []int{6, 7, 8}
 
-	//we will iterate using range
-
-	// for _, num := range nums {
-	// 	fmt.Println(num)           --op 6 7 8
-	// }
-
-	
-	// for i, num := range nums {    -- i is basically Index 
-	// 	fmt.Println(num, i)
-	// }
-
-	// sum:=0
-	// for _, num := range nums {
-	// 	sum=sum+num
-	// }
-   // fmt.Println(sum)         --21
-
-	
-	// m := map[string]string{"fname": "john", "lname": "doe"}
-
-	// for k, v := range m {
-	// 	fmt.Println(k, v)
-	// 	fmt.Println(k, v)
-	// }
-
-	// for k := range m {    --it will give only keys
-	// 	fmt.Println(k)
-	// }
-
-	// unicode code point rune
-	// starting byte of rune
-	// 300 -> 1 byte , 2 byte
-// for i, c := range "golang" {
-// 		fmt.Println(i, c)             --this will give unicode of the cahr
-// 	}
-	
-	for i, c := range "golang" {
-		fmt.Println(i, string(c))   --this will give char
+	for i := 0; i < len(nums); i++ {
+		fmt.Println(nums[i]) // 6 7 8
 	}
 
+	// -----------------------------------
+	// 2. Iterating over a slice using range
+	// -----------------------------------
+	for _, num := range nums {
+		fmt.Println(num) // values only
+	}
+
+	for i, num := range nums {
+		fmt.Println("index:", i, "value:", num)
+	}
+
+	// -----------------------------------
+	// 3. Sum of elements
+	// -----------------------------------
+	sum := 0
+	for _, num := range nums {
+		sum = sum + num
+	}
+	fmt.Println("sum:", sum) // 21
+
+	// -----------------------------------
+	// 4. Iterating over a map
+	// -----------------------------------
+	m := map[string]string{"fname": "john", "lname": "doe"}
+
+	for k, v := range m {
+		fmt.Println("key:", k, "value:", v)
+	}
+
+	for k := range m { // only keys
+		fmt.Println("key only:", k)
+	}
+
+	// -----------------------------------
+	// 5. Iterating over a string (runes / Unicode)
+	// -----------------------------------
+	// range over string gives:
+	// i = starting byte index
+	// c = rune (Unicode code point)
+
+	for i, c := range "golang" {
+		fmt.Println(i, c) // Unicode value
+	}
+
+	for i, c := range "golang" {
+		fmt.Println(i, string(c)) // actual character
+	}
 }
+// --------------------------
+🔹 Slice / Array
+for i, v := range nums
+
+
+i → index
+
+v → value
+
+🔹 Map
+for k, v := range m
+
+
+order is not guaranteed
+
+🔹 String
+for i, c := range "golang"
+
+
+i → byte index
+
+c → rune (Unicode code point)
